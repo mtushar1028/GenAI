e93056d784c33ed5b2af3cdc5be368dece868432
Tushar <mtushar1028@gmail.com>
2024-06-14 15:00:23 +0530

    Added one product (product 4)

--- eCommerce/js/product.js
+++ eCommerce/js/product.js
@@ -2,6 +2,7 @@ const products = [
     { id: 1, name: 'Product 1', price: 10, description: 'Description for product 1', image: 'images/product1.jpg' },
     { id: 2, name: 'Product 2', price: 20, description: 'Description for product 2', image: 'images/product2.jpg' },
     { id: 3, name: 'Product 3', price: 30, description: 'Description for product 3', image: 'images/product3.jpg' },
+    { id: 4, name: 'Product 4', price: 40, description: 'Description for product 4', image: 'images/product4.jpg' },
 ];
 
 function loadProducts() {
